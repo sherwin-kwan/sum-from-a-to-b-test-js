441513c133525c7925e6f69e1acc83f54a8cfe9e
Sherwin <sherwin.kwan92@gmail.com>
2020-09-18 14:23:36 -0700

    created recursive sum function

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (fromN > toN) {
+    const swap = toN;
+    toN = fromN;
+    fromN = swap;
+  }; // Ensures the two values are in the right order
+  if (fromN === toN) {
+    return toN;
+  } else {
+    return toN + sum(fromN, toN - 1);
+  }
 }
 
 module.exports = sum;
